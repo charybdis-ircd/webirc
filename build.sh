da2ecbb3cb97ff502d5460f5bcc2dbb8d8e3090c
William Pitcock <nenolod@dereferenced.org>
2016-04-10 01:16:33 -0500

    build: properly build the dist js

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ mkdir -p dist/js
 echo "var webirc = {};" > dist/js/webirc.js
 for i in parse.js client.js; do
     echo $i
-    cat src/*.js >> dist/js/webirc.js
+    cat src/$i >> dist/js/webirc.js
 done
 
 echo "all done"
